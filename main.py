278e32117b4800e51669f308ff2c9570d33e78be
Viktor Bochagov <vitekbochagov@mail.ru>
2024-12-18 18:28:04 +0500

    update functionality

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from src import masks
+from src import masks, widget
 
 
 print(masks.get_mask_card_number(7000792289606361))
 print(masks.get_mask_account(73654108430135874305))
+print(widget.mask_account_card("Visa Platinum 7000792289606361"))
+print(widget.mask_account_card("Maestro 7000792289606361"))
+print(widget.mask_account_card("Счет 73654108430135874305"))
+print(widget.get_date())
---
+++ src/widget.py
@@ -0,0 +1,17 @@
+from src.masks import get_mask_card_number, get_mask_account
+from typing import Union
+
+
+def mask_account_card(user_input: Union[str, int]) -> str:
+    """Маскирует информацию о картах и счетах"""
+    if "Счет" in user_input:
+        return f"Счет {get_mask_account(user_input)}"
+    else:
+        card_numbers = get_mask_card_number(user_input[-16:])
+        card_mask = user_input.replace(user_input[-16:], card_numbers)
+        return card_mask
+
+
+def get_date(date: str) -> str:
+    """Возвращает дату в формате ДД.ММ.ГГГГ"""
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
