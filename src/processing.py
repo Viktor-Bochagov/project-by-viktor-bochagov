6b7f41ef16434e7f2e71749f400eb611a941d4fe
Viktor Bochagov <vitekbochagov@mail.ru>
2024-12-26 19:08:27 +0500

    add function

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,14 @@
-def filter_by_state():
-    pass
+from typing import Dict, List
+
+
+def filter_by_state(last_dict: List[Dict], value_key: str = "EXECUTED") -> List[Dict]:
+    """Возвращает новый список словарей, содержащий словари соответствующих ключ"""
+
+    new_list_dict = []
+    for every_dict in last_dict:
+        if every_dict["state"] == value_key:
+            new_list_dict.append(every_dict)
+    return new_list_dict
 
 
 def sort_by_date():
