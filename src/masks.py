eb4c6beca14abc995a3b73ba31d9c1484258a479
Viktor Bochagov <vitekbochagov@mail.ru>
2025-01-03 14:07:40 +0500

    add tests for masks

--- src/masks.py
+++ src/masks.py
@@ -4,16 +4,18 @@ from typing import Union
 def get_mask_card_number(card_num: Union[str, int]) -> str:
     """Возвращает маску номера по правилу XXXX XX ** **** XXXX"""
 
-    mask_card_num = str(card_num)
-    mask_card_num = mask_card_num[:4] + " " + mask_card_num[4:6] + "** **** " + mask_card_num[-4:]
-
-    return mask_card_num
+    if card_num == "":
+        raise TypeError("Отсутствует обязательный аргумент при вводе номера карты")
+    elif len(card_num) != 16:
+        raise IndexError("Не соответствует длина строки номера карты")
+    return f"{card_num[:4]} {card_num[4:6]}** **** {card_num[-4:]}"
 
 
 def get_mask_account(account: Union[str, int]) -> str:
     """Возвращает маску номера по правилу **XXXX"""
 
-    mask_account = str(account)
-    mask_account = "**" + mask_account[-4:]
-
-    return mask_account
+    if account == "":
+        raise TypeError("Отсутствует обязательный аргумент при вводе номера счёта")
+    elif len(account) != 20:
+        raise IndexError("Не соответствует длина строки номера счёта")
+    return f"**{account[-4:]}"
