00d3517416d5ea20f3fb886e3ffe19cd7329b958
Viktor Bochagov <vitekbochagov@mail.ru>
2025-01-04 13:02:29 +0500

    add tests for processing

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from src import masks, widget
+from src.masks import get_mask_account, get_mask_card_number
 from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
-
-print(masks.get_mask_card_number(7000792289606361))
-print(masks.get_mask_account(73654108430135874305))
-print(widget.mask_account_card("Visa Platinum 7000792289606361"))
-print(widget.mask_account_card("Maestro 7000792289606361"))
-print(widget.mask_account_card("Счет 73654108430135874305"))
-print(widget.get_date("2024-03-11T02:26:18.671407"))
+print(get_mask_card_number('7000792289606361'))
+print(get_mask_account('73654108430135874305'))
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Maestro 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_date("2024-03-11T02:26:18.671407"))
 print(
         filter_by_state(
             [
