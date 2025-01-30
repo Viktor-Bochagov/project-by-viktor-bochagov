2e639a5966622cfd3b800d7835ff67aa026dca99
Viktor Bochagov <vitekbochagov@mail.ru>
2025-01-30 18:20:49 +0500

    fixed utils

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 from typing import Any
 
+
 ROOT_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = ROOT_DIR / "data"
 
@@ -12,12 +13,15 @@ def operation(open_file: Any) -> Any:
 
     open_file += ".json"
     try:
-        with open(DATA_DIR / open_file, "r", encoding="utf-8") as jf:
-            data = json.load(jf)
+        with open(DATA_DIR / open_file, "r", encoding="utf-8") as f:
+            data = json.load(f)
     except Exception as e:
         print(f"Ошибка {e}")
         return []
     except FileNotFoundError as e:
-        print(f"Ошибка {e}")
+        print(f"Ошибка: Файл не найден {e}")
+        return []
+    except json.JSONDecodeError as e:
+        print(f"Ошибка декодирования файла {e}")
         return []
     return data
