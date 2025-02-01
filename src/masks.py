3a0e8643399c51d01c2b74313fa8da319c44101e
Viktor Bochagov <vitekbochagov@mail.ru>
2025-02-01 17:44:41 +0500

    написаны логи для модуля utils

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,23 @@
+import logging
 from typing import Union
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("./logs/mask.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(card_num: Union[str]) -> str:
     """Возвращает маску номера по правилу XXXX XX ** **** XXXX"""
 
+    logger.info(f"Данная функция {get_mask_card_number.__name__} маскирует номер карты по типу 1234 56** **** 4567")
     if card_num == "":
+        logger.error(f"Произошла ошибка типа {TypeError}: отсутствует аргумент ввода")
         raise TypeError("Отсутствует обязательный аргумент при вводе номера карты")
     elif len(card_num) != 16:
+        logger.error(f"Произошла ошибка типа {IndexError}: не соответствует длина строки")
         raise IndexError("Не соответствует длина строки номера карты")
     return f"{card_num[:4]} {card_num[4:6]}** **** {card_num[-4:]}"
 
@@ -14,8 +25,11 @@ def get_mask_card_number(card_num: Union[str]) -> str:
 def get_mask_account(account: Union[str]) -> str:
     """Возвращает маску номера по правилу **XXXX"""
 
+    logger.info(f"Данная функция {get_mask_account.__name__} маскирует номер счёта по типу **1234")
     if account == "":
+        logger.error(f"Произошла ошибка типа {TypeError}: отсутствует аргумент ввода")
         raise TypeError("Отсутствует обязательный аргумент при вводе номера счёта")
     elif len(account) != 20:
+        logger.error(f"Произошла ошибка типа {IndexError}: не соответствует длина строки")
         raise IndexError("Не соответствует длина строки номера счёта")
     return f"**{account[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
 import json
+import logging
 from pathlib import Path
 from typing import Any
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("./logs/utils.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
@@ -13,15 +21,21 @@ def operation(open_file: Any) -> Any:
 
     open_file += ".json"
     try:
+        logger.info(
+            f"Данная функция {operation.__name__} принимает на вход имя JSON-файла и возвращает список словарей"
+        )
         with open(DATA_DIR / open_file, "r", encoding="utf-8") as f:
             data = json.load(f)
     except Exception as e:
+        logger.error(f"Произошла ошибка типа {e}")
         print(f"Ошибка {e}")
         return []
     except FileNotFoundError as e:
+        logger.error(f"Произошла ошибка: Файл не найден {e}")
         print(f"Ошибка: Файл не найден {e}")
         return []
     except json.JSONDecodeError as e:
+        logger.error(f"Произошла ошибка декодирования файла {e}")
         print(f"Ошибка декодирования файла {e}")
         return []
     return data
